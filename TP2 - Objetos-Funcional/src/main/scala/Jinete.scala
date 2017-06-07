7f4e4d438c4262d67577f3973761048c1530b47f
degraziafm <degraziafm@gmail.com>
2017-06-07 15:18:52 -0300

    metodos jinete

--- TP2 - Objetos-Funcional/src/main/scala/Jinete.scala
+++ TP2 - Objetos-Funcional/src/main/scala/Jinete.scala
@@ -1,5 +1,13 @@
 package main.scala
+import items.item._
+import vikingos.Vikingo
+import dragones.Dragon
 
-case class Jinete() {
+case class Jinete(unDragon:Dragon) 
+  extends Vikingo(peso: Int, item: Item, barbarosidad: Int, velocidad: Int) {
+  
+  override def puedeCargar: Int = this.peso - unDragon.cuantoPuedeCargar
+  
+  def velocidad: Int = copy(velocidad = unDragon.velocidad - this.peso)
   
 }

6c19cdcf58540e6b244f4e62aeadd329bdc0ac2e
degraziafm <degraziafm@gmail.com>
2017-06-07 14:17:20 -0300

    barbarosidad

--- TP2 - Objetos-Funcional/src/main/scala/vikingos/Vikingo.scala
+++ TP2 - Objetos-Funcional/src/main/scala/vikingos/Vikingo.scala
@@ -1,10 +1,13 @@
 package main.scala.vikingos
 import items.item._
 
-case class Vikingo(peso: Int, item: Item, danio: Int, hambre: Int = 0) {
+case class Vikingo(peso: Int, item: Item, barbarosidad: Int, hambre: Int = 0) 
+{
   def tieneUn(_item: Item) = item == _item
   
-  def danio(dani: Int): Vikingo = copy(danio = dani + danio)
+  def puedeCargar: Int = peso/2 + barbarosidad
+  
+  def danio(dani: Int): Vikingo = barbarosidad + dani
   
   def hambre(hambre: Int): Vikingo = copy(hambre = hambre + danio)
 }

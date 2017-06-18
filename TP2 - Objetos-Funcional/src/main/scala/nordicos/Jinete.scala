bb7bdf2f477745ae1ee15e5a06b7e816404bfc6e
degraziafm@gmail.com <degraziafm@gmail.com>
2017-06-18 19:25:43 -0300

    errores

--- TP2 - Objetos-Funcional/src/main/scala/nordicos/Jinete.scala
+++ TP2 - Objetos-Funcional/src/main/scala/nordicos/Jinete.scala
@@ -2,12 +2,14 @@ package main.scala
 import items.item._
 import dragones.Dragon
 
-case class Jinete(peso: Int, item: Item, barbarosidad: Int, hambre: Int, velocidad: Int, unDragon:Dragon) 
+case class Jinete(peso: Int, item: Item, barbarosidad: Int, hambre: Int, vel: Int, unDragon:Dragon) 
      extends Nordico(item: Item) 
 {  
   def puedeCargar: Int = peso - unDragon.cuantoPuedeCargar
   
   var danio = barbarosidad + unDragon.danio
   
-  def velocidad2: Jinete = copy(velocidad = unDragon.velocidad - peso)  
+  def hambre(inc: Int): Jinete = copy(hambre = hambre + inc)
+  
+  def velocidad: Int = unDragon.velocidad - peso
 }
--- TP2 - Objetos-Funcional/src/main/scala/nordicos/Nordico.scala
+++ TP2 - Objetos-Funcional/src/main/scala/nordicos/Nordico.scala
@@ -1,9 +1,12 @@
 package main.scala
 import items.item._
 
-abstract class Nordico(item: Item) {
-
-  val hambre: Int = 0
+abstract class Nordico(item: Item) 
+{
+  def hambre: Int
+  
+  def velocidad: Int
+  
   def tieneUn(_item: Item) = item == _item
   
   def puedeCargar: Int
